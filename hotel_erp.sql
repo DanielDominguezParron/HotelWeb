469da4349733c20f5be049a8969442cc9e54794d
Dani <danixntl@gmail.com>
2020-01-15 18:35:44 +0100

    bd actualizda para calendario

--- hotel_erp.sql
+++ hotel_erp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-01-2020 a las 22:04:53
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 7.3.2
+-- Tiempo de generación: 15-01-2020 a las 18:34:22
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `hotel_erp`
 --
-CREATE DATABASE IF NOT EXISTS `hotel_erp` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `hotel_erp`;
 
 -- --------------------------------------------------------
 
@@ -30,13 +28,11 @@ USE `hotel_erp`;
 -- Estructura de tabla para la tabla `clientes`
 --
 
-DROP TABLE IF EXISTS `clientes`;
-CREATE TABLE IF NOT EXISTS `clientes` (
+CREATE TABLE `clientes` (
   `DNI` varchar(9) COLLATE utf8_spanish_ci NOT NULL,
   `name` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `surname` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
-  `mail` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  PRIMARY KEY (`DNI`)
+  `mail` varchar(50) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -53,18 +49,16 @@ INSERT INTO `clientes` (`DNI`, `name`, `surname`, `mail`) VALUES
 -- Estructura de tabla para la tabla `habitaciones`
 --
 
-DROP TABLE IF EXISTS `habitaciones`;
-CREATE TABLE IF NOT EXISTS `habitaciones` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `habitaciones` (
+  `id` int(11) NOT NULL,
   `name` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `planta` set('1','2','3','4') COLLATE utf8_spanish_ci NOT NULL,
   `number` int(3) NOT NULL,
   `price` double NOT NULL,
   `description` varchar(500) COLLATE utf8_spanish_ci NOT NULL,
   `photo` varchar(300) COLLATE utf8_spanish_ci NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `habitaciones`
@@ -84,16 +78,19 @@ INSERT INTO `habitaciones` (`id`, `name`, `planta`, `number`, `price`, `descript
 -- Estructura de tabla para la tabla `reservas`
 --
 
-DROP TABLE IF EXISTS `reservas`;
-CREATE TABLE IF NOT EXISTS `reservas` (
-  `IdReserva` int(11) NOT NULL AUTO_INCREMENT,
-  `IdCliente` int(11) NOT NULL,
+CREATE TABLE `reservas` (
+  `IdReserva` int(11) NOT NULL,
+  `IdCliente` varchar(11) COLLATE utf8_spanish_ci NOT NULL,
   `IdHabitacion` int(11) NOT NULL,
-  `TotalPrice` double NOT NULL,
-  PRIMARY KEY (`IdReserva`),
-  KEY `IdHabitacion` (`IdHabitacion`),
-  KEY `IdCliente` (`IdCliente`)
+  `TotalPrice` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `reservas`
+--
+
+INSERT INTO `reservas` (`IdReserva`, `IdCliente`, `IdHabitacion`, `TotalPrice`) VALUES
+(1, '78912345y', 1, 300);
 
 -- --------------------------------------------------------
 
@@ -101,14 +98,19 @@ CREATE TABLE IF NOT EXISTS `reservas` (
 -- Estructura de tabla para la tabla `reserva_habitaciones`
 --
 
-DROP TABLE IF EXISTS `reserva_habitaciones`;
-CREATE TABLE IF NOT EXISTS `reserva_habitaciones` (
+CREATE TABLE `reserva_habitaciones` (
   `BookingDate` date NOT NULL,
+  `LeavingDate` date NOT NULL,
   `IdReserva` int(11) NOT NULL,
-  `IdHabitacion` int(11) NOT NULL,
-  KEY `IdReserva` (`IdReserva`),
-  KEY `IdHabitacion` (`IdHabitacion`)
+  `IdHabitacion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `reserva_habitaciones`
+--
+
+INSERT INTO `reserva_habitaciones` (`BookingDate`, `LeavingDate`, `IdReserva`, `IdHabitacion`) VALUES
+('2020-01-10', '2020-01-14', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -116,15 +118,12 @@ CREATE TABLE IF NOT EXISTS `reserva_habitaciones` (
 -- Estructura de tabla para la tabla `reseñas`
 --
 
-DROP TABLE IF EXISTS `reseñas`;
-CREATE TABLE IF NOT EXISTS `reseñas` (
-  `IdReseña` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `reseñas` (
+  `IdReseña` int(11) NOT NULL,
   `IdCliente` int(11) NOT NULL,
   `IdReserva` int(11) NOT NULL,
   `Description` varchar(500) COLLATE utf8_spanish_ci NOT NULL,
-  `Opinion` tinyint(1) NOT NULL,
-  PRIMARY KEY (`IdReseña`),
-  KEY `IdReserva` (`IdReserva`)
+  `Opinion` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -133,14 +132,12 @@ CREATE TABLE IF NOT EXISTS `reseñas` (
 -- Estructura de tabla para la tabla `trabajadores`
 --
 
-DROP TABLE IF EXISTS `trabajadores`;
-CREATE TABLE IF NOT EXISTS `trabajadores` (
+CREATE TABLE `trabajadores` (
   `name` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `surname` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `mail` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `password` varchar(14) COLLATE utf8_spanish_ci NOT NULL,
-  `DNI` varchar(9) COLLATE utf8_spanish_ci NOT NULL,
-  PRIMARY KEY (`DNI`)
+  `DNI` varchar(9) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -154,6 +151,72 @@ INSERT INTO `trabajadores` (`name`, `surname`, `mail`, `password`, `DNI`) VALUES
 ('Dani', 'Velazquez', 'danivel@gmail.com', '543219876', '32109876D');
 
 --
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`DNI`);
+
+--
+-- Indices de la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`IdReserva`),
+  ADD KEY `IdHabitacion` (`IdHabitacion`),
+  ADD KEY `IdCliente` (`IdCliente`);
+
+--
+-- Indices de la tabla `reserva_habitaciones`
+--
+ALTER TABLE `reserva_habitaciones`
+  ADD KEY `IdReserva` (`IdReserva`),
+  ADD KEY `IdHabitacion` (`IdHabitacion`);
+
+--
+-- Indices de la tabla `reseñas`
+--
+ALTER TABLE `reseñas`
+  ADD PRIMARY KEY (`IdReseña`),
+  ADD KEY `IdReserva` (`IdReserva`);
+
+--
+-- Indices de la tabla `trabajadores`
+--
+ALTER TABLE `trabajadores`
+  ADD PRIMARY KEY (`DNI`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `IdReserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `reseñas`
+--
+ALTER TABLE `reseñas`
+  MODIFY `IdReseña` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restricciones para tablas volcadas
 --
 
@@ -162,7 +225,7 @@ INSERT INTO `trabajadores` (`name`, `surname`, `mail`, `password`, `DNI`) VALUES
 --
 ALTER TABLE `reserva_habitaciones`
   ADD CONSTRAINT `reserva_habitaciones_ibfk_1` FOREIGN KEY (`IdReserva`) REFERENCES `reservas` (`IdReserva`),
-  ADD CONSTRAINT `reserva_habitaciones_ibfk_2` FOREIGN KEY (`IdHabitacion`) REFERENCES `habitaciones` (`Id`);
+  ADD CONSTRAINT `reserva_habitaciones_ibfk_2` FOREIGN KEY (`IdHabitacion`) REFERENCES `habitaciones` (`id`);
 
 --
 -- Filtros para la tabla `reseñas`
